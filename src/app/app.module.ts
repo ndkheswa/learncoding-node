23e98fd4c23767d40f60dd34945e7477d33d9217
Ndoda Kheswa (ZA) <Ndoda.Kheswa@absa.africa>
2020-05-19 01:33:30 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({ autoLoadEntities: true }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
